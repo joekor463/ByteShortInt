6e09dc9642c1a6e0cebca6d122d9465a7deb31cb
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 08:18:12 +0800

    operation with digit

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //int Myvalue = 1000;
+        byte Mybyte1 = 32;
+        short Myshort1 = 54;
+        int Myint1 = 320001;
+        long Mylong1 = 50000L + 10L * (Mybyte1 + Myshort1 + Myint1);
+        ("My cifir =" + Mylong1);
+
+    }
+}
